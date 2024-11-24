835c36b6ec31751cbdbb1cce2a1b5a2c8b4b5b0d
jhpark223 <thesound08@naver.com>
2024-11-25 01:20:22 +0900

    generate dataset and train crossencoder

--- train_crossencoder.py
+++
@@ -1,9 +0,0 @@
-"""
-TODO
-1. Load pretrained model from huggingface
-2. Finetune pretrained model by custom dataset
-
-custom dataset은...
-1. document로부터 query를 생성하는 모델을 사용하여 query->document pair 구성
-2. DPR처럼 crossencoder를 학습
-"""
